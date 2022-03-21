cbae7ddd21abd0fd69120870b35b74a64c36ac25
ElectronSpark <820461770@qq.com>
2022-03-21 22:53:04 +0800

    build x86_64

--- cpu/x86_64/include/cpu_specific.h
+++ cpu/x86_64/include/cpu_specific.h
@@ -4,6 +4,38 @@
 
 #include <cpu_types.h>
 
+/* 下面几个函数用汇编实现 */
+extern cr_context_t *__cr_switch_to(void *prev, void *next,
+                                    cr_context_t *last);
+extern void __cr_context_init(void *arg1, void *arg2,
+                              void *stack_end, void *func_entry);
+extern void *__cr_context_save(void *regs);
 
+/* 上下文切换的 cpu 特定实现 */
+static inline cr_context_t *cr_switch_to(cr_context_t *prev,
+                                         cr_context_t *next)
+{
+    if (!prev || !next) {
+        return NULL;
+    }
+
+    return __cr_switch_to(prev->gp_regs, next->gp_regs, prev);
+}
+
+/* 初始化协程上下文 */
+static inline void cr_context_init(
+    cr_context_t *self,
+    void *stack,
+    size_t stack_size,
+    void *func_entry,
+    void *arg1,
+    void *arg2
+)
+{
+    /* 如果上一个协程是主协程，说明当前协程是新创建的协程 */
+    if (__cr_context_save(self->gp_regs) == self->gp_regs) {
+        __cr_context_init(arg1, arg2, stack + stack_size, func_entry);
+    }
+}
 
 #endif /* __CR_CPU_SPECIFIC_H__ */
--- cpu/x86_64/include/cpu_types.h
+++ cpu/x86_64/include/cpu_types.h
@@ -9,10 +9,12 @@
 #define CR_CPU_GP_REGS          16
 
 /* 协程寄存器上下文 */
+#ifndef __ASM__
 struct cr_context_struct {
     void    *gp_regs[CR_CPU_GP_REGS];
 };
 typedef struct cr_context_struct    cr_context_t;
+#endif /* __ASM__ */
 
 /* 所有通用寄存器在上下文中的索引 */
 #define CR_CPU_INDEX_RAX        0
@@ -34,5 +36,12 @@ typedef struct cr_context_struct    cr_context_t;
 
 /* 栈寄存器在上下文中的索引 */
 #define CR_CPU_INDEX_SP         CR_CPU_INDEX_RSP
+#define CR_CPU_INDEX_RET        CR_CPU_INDEX_RAX
+#define CR_CPU_INDEX_ARG0       CR_CPU_INDEX_RDI
+#define CR_CPU_INDEX_ARG1       CR_CPU_INDEX_RSI
+#define CR_CPU_INDEX_ARG2       CR_CPU_INDEX_RDX
+#define CR_CPU_INDEX_ARG3       CR_CPU_INDEX_RCX
+#define CR_CPU_INDEX_ARG4       CR_CPU_INDEX_R8
+#define CR_CPU_INDEX_ARG5       CR_CPU_INDEX_R9
 
 #endif /* __CR_CPU_TYPES__ */
