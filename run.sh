72a12af5a86c643311ba24e9eae723f18890445b
ElectronSpark <820461770@qq.com>
2022-05-03 23:20:41 +0800

    benchmark

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+if [ -f ./log.txt ]; then
+    rm ./log.txt
+fi
 for (( c=0; c<=20; c++ ))
 do
     for (( d=0; d<=20; d++ ))
     do
-        ./example/test.elf $(( 2**c ))
+        ./example/test.elf $(( 2**c )) >> log.txt
     done
 done 
